1045f02ae228c1dcdd8333e3df792ac0b134d74e
AmericanWonton <jbkeller0303@gmail.com>
2020-03-18 18:17:30 -0500

    Deployed to Google cloud...now I need to point it to my domain and project.

--- project/main.go
+++ project/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 )
 
-func init() {
+func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s ", port)
+	}
+
+	log.Printf("listenting on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
