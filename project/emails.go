21cdbe0380a6d920c826cb966d4a7abe05db87c2
missusdever <jbkeller0303@gmail.com>
2024-09-17 14:10:17 -0500

    Looks good, adding in

--- project/emails.go
+++ project/emails.go
@@ -1,27 +1,22 @@
 package main
 
 import (
-	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"time"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
-	"google.golang.org/api/option"
+	"github.com/go-mail/mail"
+
 )
-
-var GmailService *gmail.Service //This gets initialized in init
 
 var theClientID string
 var theClientSecret string
 var theAccessToken string
 var theRefreshToken string
+var myemailAddress string
+var myemailBKPAddress string
+var myEmailPassword string
 
 //Declare DataType from Ajax
 type UserEmail struct {
@@ -47,13 +42,45 @@ func emailSubmit(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println(err)
 		}
+
 		//Unmarshal Data
 		var emailPosted UserEmail
 		json.Unmarshal(bs, &emailPosted)
-		//Get values
-		goodEmail, theErr := signUpUserEmail(emailPosted)
-		//Compile Response accordingly
-		if goodEmail == true {
+
+		m := mail.NewMessage()
+
+		m.SetHeader("From", emailPosted.Email)
+
+		m.SetHeader("To", myemailAddress, myemailBKPAddress)
+
+		m.SetHeader("Subject", emailPosted.Subject)
+
+		m.SetBody("text/html", emailPosted.Message)
+
+		//m.Attach("lolcat.jpg")
+
+		d := mail.NewDialer("smtp.gmail.com", 587, myemailAddress, myEmailPassword)
+
+		// Send the email to Kate, Noah and Oliver.
+
+		if err := d.DialAndSend(m); err != nil {
+			type successMSG struct {
+				Message     string `json:"Message"`
+				SuccessNum  int    `json:"SuccessNum"`
+				RedirectURL string `json:"RedirectURL"`
+			}
+			msgSuccess := successMSG{
+				Message:     "Email issue! Please review information sent\n" + err.Error(),
+				SuccessNum:  1,
+				RedirectURL: "http://" + "localhost:" + port,
+			}
+
+			theJSONMessage, err := json.Marshal(msgSuccess)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Fprint(w, string(theJSONMessage))
+		} else {
 			type successMSG struct {
 				Message     string `json:"Message"`
 				SuccessNum  int    `json:"SuccessNum"`
@@ -71,89 +98,7 @@ func emailSubmit(w http.ResponseWriter, r *http.Request) {
 			}
 			fmt.Println("Email successfully submitted.")
 			fmt.Fprint(w, string(theJSONMessage))
-		} else {
-			type successMSG struct {
-				Message     string `json:"Message"`
-				SuccessNum  int    `json:"SuccessNum"`
-				RedirectURL string `json:"RedirectURL"`
-			}
-			msgSuccess := successMSG{
-				Message:     "Email issue! Please review information sent\n" + theErr.Error(),
-				SuccessNum:  1,
-				RedirectURL: "http://" + "localhost:" + port,
-			}
-
-			theJSONMessage, err := json.Marshal(msgSuccess)
-			if err != nil {
-				fmt.Println(err)
-			}
-			fmt.Fprint(w, string(theJSONMessage))
 		}
+
 	}
-}
-
-//Initialized at begininning of program
-func OAuthGmailService() {
-	config := oauth2.Config{
-		ClientID:     theClientID,
-		ClientSecret: theClientSecret,
-		Endpoint:     google.Endpoint,
-		RedirectURL:  "http://" + "localhost:" + port,
-	}
-
-	token := oauth2.Token{
-		AccessToken:  theAccessToken,
-		RefreshToken: theRefreshToken,
-		TokenType:    "Bearer",
-		Expiry:       time.Now(),
-	}
-
-	var tokenSource = config.TokenSource(context.Background(), &token)
-
-	srv, err := gmail.NewService(context.Background(), option.WithTokenSource(tokenSource))
-	if err != nil {
-		errMsg := "Unable to retrieve Gmail client: " + err.Error()
-		fmt.Println(errMsg)
-		log.Println(errMsg)
-	}
-
-	GmailService = srv
-	if GmailService != nil {
-		succMsg := "Email service is initialized"
-		fmt.Println(succMsg)
-		//log.Println(succMsg)
-	}
-}
-
-//Attempts to send an email to User
-func signUpUserEmail(theEmail UserEmail) (bool, error) {
-	fmt.Println("DEBUG: Sending email in signUpUpserEmail")
-	goodEmailSend := true
-	theMessage := "Recieved a message from " + theEmail.FName + " " + theEmail.LName +
-		" at " + theEmail.Email + ":\n\n" + theEmail.Message + "\n\n" +
-		"Phone Num is: " + theEmail.AreaCode + "-" + theEmail.PhoneNum1 + "-" +
-		theEmail.PhoneNum2 + "-" + theEmail.PhoneNum3 + "\n" +
-		"Email: " + theEmail.Email
-	theSubject := theEmail.Subject
-
-	var message gmail.Message
-
-	emailTo := "To: " + "jbkeller0303@gmail.com" + "\r\n"
-	subject := "Subject: " + theSubject + "\n"
-	mime := "MIME-version: 1.0;\nContent-Type: text/plain; charset=\"UTF-8\";\n\n"
-	msg := []byte(emailTo + subject + mime + "\n" + theMessage)
-
-	message.Raw = base64.URLEncoding.EncodeToString(msg)
-
-	// Send the message
-	fmt.Println("DEBUG: About to send this Gmail...")
-	_, err := GmailService.Users.Messages.Send("me", &message).Do()
-	if err != nil {
-		errMsg := "Error sending this message to the User: " + err.Error()
-		fmt.Println(errMsg)
-		log.Println(errMsg)
-		goodEmailSend = false
-	}
-
-	return goodEmailSend, err
 }
--- project/go.mod
+++ project/go.mod
@@ -3,7 +3,11 @@ module resume_site_deployment/joek_resume_site/project
 go 1.15
 
 require (
+	github.com/go-mail/mail v2.3.1+incompatible // indirect
 	github.com/gorilla/mux v1.8.0
 	golang.org/x/oauth2 v0.0.0-20210220000619-9bb904979d93
 	google.golang.org/api v0.40.0
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
